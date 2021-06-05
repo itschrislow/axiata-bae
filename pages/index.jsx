75de8d8450e26b19ec73374dc7a438643cd931d2
chris0610 <chrislow360@gmail.com>
2021-06-05 16:55:19 +0800

    add login component

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Login from "../src/components/Login";
+
+const Home = () => {
+  return (
+    <Login />
+  )
+}
+
+export default Home;
---
+++ src/components/Login.jsx
@@ -0,0 +1,143 @@
+import { useState } from "react"
+import { useRouter } from "next/router";
+
+export const TYPE = {
+  Student: "student",
+  Teacher: "teacher",
+}
+
+export default function Login() {
+  const router = useRouter();
+  const [type, setType] = useState(TYPE.Student);
+
+  const handleLogin = () => {
+    if (type === TYPE.Student) router.push("/student")
+    if (type === TYPE.Teacher) router.push("/teacher")
+  }
+
+  return (
+    <div className="min-h-screen bg-white flex">
+      <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <img
+              className="h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-sm text-gray-600">
+              I am a
+            </p>
+            <div className="mt-4 grid grid-cols-2">
+              <div className="flex items-center">
+                <input
+                  id="student"
+                  name="user_type"
+                  type="radio"
+                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                  checked={type === TYPE.Student}
+                  onClick={() => setType(TYPE.Student)}
+                />
+                <label htmlFor="push_everything" className="ml-3 block text-sm font-medium text-gray-700">
+                  Student
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="teacher"
+                  name="user_type"
+                  type="radio"
+                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                  checked={type === TYPE.Teacher}
+                  onClick={() => setType(TYPE.Teacher)}
+                />
+                <label htmlFor="push_email" className="ml-3 block text-sm font-medium text-gray-700">
+                  Educator
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {/* LOGIN DETAILS */}
+          <div className="mt-8">
+            <div className="mt-6">
+              <form action="#" method="POST" className="space-y-6">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="space-y-1">
+                  <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                    Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember_me"
+                      name="remember_me"
+                      type="checkbox"
+                      className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                    />
+                    <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                      Remember me
+                    </label>
+                  </div>
+
+                  <div className="text-sm">
+                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                      Forgot your password?
+                    </a>
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    onClick={handleLogin}
+                    type="button"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block relative w-0 flex-1">
+        <img
+          className="absolute inset-0 h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+  )
+}
