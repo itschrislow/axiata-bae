37c240c416c862c1c4ddf639c7527ee63eee48f6
chris0610 <chrislow360@gmail.com>
2021-06-05 17:56:01 +0800

    query qaDeck with airtable api

--- pages/student/index.jsx
+++ pages/student/index.jsx
@@ -1,33 +1,27 @@
+import { useEffect, useState } from "react";
 import Nav from "../../src/components/Nav";
 import ScenarioCard from "../../src/components/ScenarioCard";
 
-const scenarios = [
-  {
-    title: 'Body Shaming',
-    link: 'body-shaming',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'Physical Bullying',
-    link: 'physical-bullying',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-  {
-    title: 'Name Calling',
-    link: 'name-calling',
-    source:
-      'https://images.unsplash.com/photo-1582053433976-25c00369fc93?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80',
-  },
-]
-
 const Student = () => {
+  const [scenarios, setScenarios] = useState([]);
+
+  const fetchScenarios = async () => {
+    await fetch(`
+      ${process.env.NEXT_PUBLIC_AIRTABLE_API_URL}qaDeck?api_key=${process.env.NEXT_PUBLIC_AIRTABLE_API_KEY}&view=Grid%20view
+    `)
+      .then(res => res.json())
+      .then(res => setScenarios(res.records))
+  }
+
+  useEffect(() => {
+    fetchScenarios();
+  }, [])
+
   return (
     <Nav>
       <div className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6">
         {scenarios?.map(scenario => (
-          <ScenarioCard scenario={scenario} />
+          <ScenarioCard scenario={scenario.fields} />
         ))}
       </div>
     </Nav>
--- src/components/ScenarioCard.jsx
+++ src/components/ScenarioCard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const ScenarioCard = ({ scenario }) => {
   return (
-    <Link href={`/student/${scenario.link}`}>
+    <Link href={`/student/scenario?qaDeck=${scenario.queryParam}`}>
       <div className="relative text-center">
         <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
           <img
@@ -12,7 +12,7 @@ const ScenarioCard = ({ scenario }) => {
           />
         </div>
         <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
-          {scenario.title}
+          {scenario.deckName}
         </p>
       </div>
     </Link>
